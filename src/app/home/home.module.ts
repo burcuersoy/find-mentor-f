73392d7b891d075fe1e21628e40b254fb2473930
m0dexl <yahya.altungul@gmail.com>
2023-10-03 14:33:54 +0300

    hero component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    HeroComponent
   ],
   imports: [
     CommonModule
